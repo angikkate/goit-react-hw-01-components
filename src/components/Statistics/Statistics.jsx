86a37b01580acf03d33820660419c5c76de089d9
angikkate <angikkate@gmail.com>
2023-04-23 19:29:51 +0300

    bugfix page

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -4,14 +4,14 @@ import css from './Statistics.module.css';
 export const Statistics = ({ title, stats }) => {
   return (
     <section className={css.statistics}>
-      <h2 className={css.title}>Upload stats</h2>
+      {title && <h2 className={css.title}>{title}</h2>}
 
       <ul className={css.statList}>
         {stats.map( ({id, label, percentage}) => {
           return (
             <li className={css.item} style={{backgroundColor: getRandomHexColor()}} key={id}>
               <span className={css.label}>{label}</span>
-              <span className={css.percentage}>{percentage}</span>
+              <span className={css.percentage}>{percentage}%</span>
             </li>
           );
         })}
